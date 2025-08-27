c2c26df99b4196d8423cf527b93a29aaca22deb3
bogdan <bogdandidyc91@gmail.com>
2025-08-27 16:44:26 +0300

    дз№3

--- homeWork.js
+++ homeWork.js
@@ -1,11 +1,43 @@
+const name = "bogdan";
+console.log(typeof name);
+
+let age = 20;
+console.log(typeof age);
+
+const coolVideo = true;
+const badVideo = false;
+
+console.log(typeof coolVideo);
+console.log(typeof badVideo);
+
+let price;
+console.log(typeof price);
+
+let a = null
+console.log(typeof a)
+
 /*
-с let можно повторно обьявить переменную поскульку let позволяет это 
-в случае с const неполучиться ибо оно не позволить обновлять значение переменно и будет выдавать ошибку
+разница между хранением по значению  и по ссылке в том что применяется зачастую к разным типап данных
+так же в храние по значению создается отдельная копия данных а 
+в хранению по ссылке сохраняется не значение а ссылка
+хранение по значинею относиться к числам стокам и т.п.
+если же брать ссылочное храниение то оно относиться к объектам массивам и фенкциям 
+примеры ниже
 */
 
-let price = 100;
+let obj1 = {name: 'bogdan'}
+let obj2 = obj1
 
-price = 150
+obj1.name = 'саша'
 
-console.log(price);
+console.log(obj1.name)
+console.log(obj2.name)
 
+
+let c = 5
+let b = c
+
+c = 10
+
+console.log(c)
+console.log(b)
