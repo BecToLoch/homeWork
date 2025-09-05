d7841ce654a1adb160aa3ec60e91b06857fbf199
bogdan <bogdandidyc91@gmail.com>
2025-09-05 22:42:37 +0300

    дз №10

--- homeWork.js
+++ homeWork.js
@@ -1,58 +1,80 @@
 /*
-const number = [1, 2, 3, 4, 5]
+const isPalindrome = (str) =>{
+  const lowerStr = str.toLowerCase()
+  const revers = lowerStr.split('').reverse().join('')
 
-function midNumber(number){
-  const  sum = number.reduce((acc, num) => acc + num, 0)
-  return sum /number.length
+  return lowerStr === revers
 }
 
-console.log(midNumber(number));
+console.log(isPalindrome('hellow'));
 */
 
 /*
-const user = {
-  name: 'bogdan',
-  age: 20 ,
-  country: 'Russia'
+const findShortestWord = (str) => {
+    const words = str.split(/\s+/);
+    let shorTest = words[0]
+    
+    for (let word of words)
+      if(word.length < shorTest.length){
+        shorTest = word
+    }
+      return shorTest
 }
 
-const {name, age, country} = user 
-
-console.log(name);
-console.log(age);
-console.log(country);
+console.log(findShortestWord('всем привет здравья желаю'));
 */
 
 /*
-const user = [ 'bogdan', [20], {city: 'Rostov'}]
+const createPhoneNumber = (number) =>{
+  const str = number.toString()
+  
+  if (str.length !== 9){
+    throw new Error ('неправильный номер')
+  }
 
- const [name, [age], {city}] = user
+  return (`8 (${str.slice(0, 3)}${str.slice(3,6)}-${str.slice(6)})`)
+}
 
-console.log(name);
-console.log(age);
-console.log(city);
+console.log(createPhoneNumber('12345689'));
 */
 
 /*
-const originalMassiv = [1, 2, 3, 4, 5]
+const masiv = [2, 3, 1, 5, 4]
 
-const copiMassiv = [0, ...originalMassiv, 6]
-console.log(copiMassiv);
+ const src = (arr) => {
+   let min = arr[0]
+   let max = arr[0]
+  
+   for (let i = 1; i < arr.length; i++){
+    if (arr[i] < min){
+      min = arr[i] 
+    }
+    if (arr[i] > max){
+      max = arr[i] 
+    }
+   }
+   return {
+    min: min,
+    max: max
+   }
+ }
+
+console.log(src(masiv));
 */
 
-/*
-function removeUser(obj, userRemove){
-  const { [userRemove]: remove, ...rest } = obj
-  return rest
-}
+const masiv = [2, 33, 42, 52, 3];
 
-const user = {
-  name: 'bogdan',
-  age: 20 ,
-  country: 'Russia'
-}
+const sorting = (arr) => {
+  const n = arr.length
 
-const result = removeUser(user, 'age')
-console.log(result);
-*/
+  for(let i = 0; i < n-1; i++){
+    for(let j = 0; j < n - 1 - i; j++){
+      if (arr[j] > arr[j + 1]){
+         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+      }
+    }
+  }
+  return arr
+};
 
+console.log(sorting(masiv));
